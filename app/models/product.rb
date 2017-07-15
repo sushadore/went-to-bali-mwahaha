4fd7141b521d5375abac87252c28803da595c950
Susha Dore <susha.dore@gmail.com>
2017-07-15 13:08:05 -0700

    refactor: add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :order_items
   validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: true
+  
 end
